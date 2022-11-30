d64f8b0ef197977d57f4b456421ba2dda2978011
sethvs <sethvs@users.noreply.github.com>
2022-11-30 16:09:02 +0300

    Add DealJobSeekerSelectForm

---
+++ cp/cp/DealJobSeekerSelectForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace cp
+{
+    partial class DealJobSeekerSelectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DealJobSeekerSelectFormDataGridView = new System.Windows.Forms.DataGridView();
+            this.DealJobSeekerSelectFormButtonOK = new System.Windows.Forms.Button();
+            this.DealJobSeekerSelectFormButtonCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DealJobSeekerSelectFormDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DealJobSeekerSelectFormDataGridView
+            // 
+            this.DealJobSeekerSelectFormDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DealJobSeekerSelectFormDataGridView.Location = new System.Drawing.Point(5, 5);
+            this.DealJobSeekerSelectFormDataGridView.Name = "DealJobSeekerSelectFormDataGridView";
+            this.DealJobSeekerSelectFormDataGridView.Size = new System.Drawing.Size(791, 411);
+            this.DealJobSeekerSelectFormDataGridView.TabIndex = 0;
+            // 
+            // DealJobSeekerSelectFormButtonOK
+            // 
+            this.DealJobSeekerSelectFormButtonOK.Location = new System.Drawing.Point(615, 426);
+            this.DealJobSeekerSelectFormButtonOK.Name = "DealJobSeekerSelectFormButtonOK";
+            this.DealJobSeekerSelectFormButtonOK.Size = new System.Drawing.Size(75, 23);
+            this.DealJobSeekerSelectFormButtonOK.TabIndex = 1;
+            this.DealJobSeekerSelectFormButtonOK.Text = "OK";
+            this.DealJobSeekerSelectFormButtonOK.UseVisualStyleBackColor = true;
+            // 
+            // DealJobSeekerSelectFormButtonCancel
+            // 
+            this.DealJobSeekerSelectFormButtonCancel.Location = new System.Drawing.Point(720, 426);
+            this.DealJobSeekerSelectFormButtonCancel.Name = "DealJobSeekerSelectFormButtonCancel";
+            this.DealJobSeekerSelectFormButtonCancel.Size = new System.Drawing.Size(75, 23);
+            this.DealJobSeekerSelectFormButtonCancel.TabIndex = 2;
+            this.DealJobSeekerSelectFormButtonCancel.Text = "Cancel";
+            this.DealJobSeekerSelectFormButtonCancel.UseVisualStyleBackColor = true;
+            // 
+            // DealJobSeekerSelectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 458);
+            this.Controls.Add(this.DealJobSeekerSelectFormButtonCancel);
+            this.Controls.Add(this.DealJobSeekerSelectFormButtonOK);
+            this.Controls.Add(this.DealJobSeekerSelectFormDataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "DealJobSeekerSelectForm";
+            this.Text = "DealJobSeekerSelectForm";
+            ((System.ComponentModel.ISupportInitialize)(this.DealJobSeekerSelectFormDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DealJobSeekerSelectFormDataGridView;
+        private System.Windows.Forms.Button DealJobSeekerSelectFormButtonOK;
+        private System.Windows.Forms.Button DealJobSeekerSelectFormButtonCancel;
+    }
+}
